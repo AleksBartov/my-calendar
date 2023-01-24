19ea733325ef1766dc7ecd2fc022c0cd85560fe9
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2023-01-25 00:27:22 +0300

    title changes two

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Добро пожаловать <a href="https://nextjs.org">Next.js!</a>
+         КАЛЕНДАРЬ
         </h1>
 
         <p className={styles.description}>
